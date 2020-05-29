b748207f94d2a489af907d56c76fd5f69fea9212
Wen Du <duwdora@gmail.com>
2020-05-28 23:03:29 -0700

    Add SoundManagerScript and placeholder for sound

---
+++ Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public AudioClip walk, jump, dash, slid, land;
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void PlaySound (string clip) {
+        switch (clip) {
+        case "walk":
+            //source.PlayOneShot(walk);
+            print("walking");
+            break;
+        case "jump":
+            //source.PlayOneShot(jump);
+            print("jump");
+            break;
+        case "dash":
+            //source.PlayOneShot(dash);
+            print("dash");
+            break;
+        case "slid":
+            //source.PlayOneShot(slid);
+            print("slide");
+            break;
+        case "land":
+            //source.PlayOneShot(land);
+            print("Landed");
+            break;
+
+        }
+    }
+}
